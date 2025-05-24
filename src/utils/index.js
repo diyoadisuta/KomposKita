f642061bde348ebe1ac4d68bbc294ece0fc28494
rhpivil <148871647+Txcgot@users.noreply.github.com>
2025-05-25 03:05:11 +0700

    model implemented on FE & fix camera bugs

---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+function convertBase64ToBlob(base64Data, contentType = '', sliceSize = 512) {
+  const byteCharacters = atob(base64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  return new Blob(byteArrays, { type: contentType });
+}
+
+export function imageUrlToBlob(imageUrl) {
+  const [meta, base64] = imageUrl.split(',');
+  const mimeMatch = meta.match(/data:(.*?);base64/);
+  const contentType = mimeMatch ? mimeMatch[1] : '';
+  return convertBase64ToBlob(base64, contentType);
+}
